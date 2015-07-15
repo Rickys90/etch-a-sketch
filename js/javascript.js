3609f65eace2d933116f5997e4986bdb4c6ad4c3
Richard T. Schell <schell.richard@yahoo.com>
2015-07-15 18:32:03 -0400

    added option 2

--- js/javascript.js
+++ js/javascript.js
@@ -4,11 +4,23 @@ $(document).ready(function() {
 	})
 });
 
+var getColor = function() {
+	var red = Math.floor((Math.random()*255)+1);
+	var green = Math.floor((Math.random()*255)+1);
+	var blue = Math.floor((Math.random()*255)+1);
+	return "rgb(" + red + "," + green + "," + blue + ")";
+
+	}
+
 var reset = function() {
-	$('div#grid').removeClass('red');
+	$('div#grid').removeClass();
 	$('div#grid').remove();
 	$('td').remove();
 	$('tr').remove();
+}
+
+var option1 = function() {
+	reset();
 	gHeight();
 	gWidth();
 	$('div#grid').mouseenter(function() {
@@ -16,7 +28,14 @@ var reset = function() {
 	})
 }
 
-
+var option2 = function() {
+	reset();
+	gHeight();
+	gWidth();
+	$('div#grid').mouseenter(function() {
+		$(this).css('background', getColor());
+	})
+}
 
 var gHeight = function(height) {
 	var height = prompt('How tall do you want the grid to be? (1-32)');
